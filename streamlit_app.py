18a55be41cf62c309ecabfdd9f37a06952ad4013
rbaggen99 <raymond.baggen@home.nl>
2026-07-28 16:25:16 +0200

    Change data selection and fruit info retrieval
    Updated the data selection from Snowflake and modified the way fruit information is fetched and displayed.

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,8 +17,14 @@ st.write('The on your smoothie will be:', name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('SEARCH_ON'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()
+
+# Convert the Snowpark dataframe to a Pandas Dataframe so we can use the LDC function
+pd_df = my_dataframe.to_panda()
+#st.dataframe(pd_df)
+#st.stop()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
@@ -32,9 +38,17 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
 
-#    st.write(ingredients_string)
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
-                    values ('""" + ingredients_string + """', '""" + name_on_order + """')"""
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+#        st.write('The search value for ', fruit_chosen, ' is ', search_on, '.')
+
+        st.subheader(fruit_chosen + 'Nutrition Information')
+        smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{search_on}")      
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)      
+
+  
+  #  st.write(ingredients_string)
+#    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
+#                    values ('""" + ingredients_string + """', '""" + name_on_order + """')"""
 #    st.write(my_insert_stmt)
 #    st.stop()
 
@@ -47,11 +61,11 @@ if ingredients_list:
         st.success('Your Smoothie is ordered!', icon="✅")
 
 import requests
-if ingredients_list:
-  ingredients_string = ''
+#if ingredients_list:
+#  ingredients_string = ''
 
-  for fruits_chosen in ingredients_list:
-    ingredients_string += fruit_chosen + ' '
-    st.subheader(fruit_chosen + 'Nutrition Information')
-    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruits_chosen)
-    sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+#  for fruits_chosen in ingredients_list:
+#    ingredients_string += fruit_chosen + ' '
+#    st.subheader(fruit_chosen + 'Nutrition Information')
+#    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruits_chosen)
+#    sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
