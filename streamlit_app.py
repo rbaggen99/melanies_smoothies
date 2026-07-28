eb93cd21c427d356a0c0572c97b99dd36e77256e
rbaggen99 <raymond.baggen@home.nl>
2026-07-28 16:02:08 +0200

    Fix API call for fruit nutrition information

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,5 +52,6 @@ if ingredients_list:
 
   for fruits_chosen in ingredients_list:
     ingredients_string += fruit_chosen + ' '
-    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+    st.subheader(fruit_chosen + 'Nutrition Information')
+    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruits_chosen)
     sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
