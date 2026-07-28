2e6ae0201708af8a594a98edf1ba156cf2b7090f
rbaggen99 <raymond.baggen@home.nl>
2026-07-28 17:13:12 +0200

    Refactor ingredient selection and display logic
    Updated spacing in subheader and added search_on display.

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,15 +20,12 @@ session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 pd_df = my_dataframe.to_pandas()
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-
+#my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
     , my_dataframe
     , max_selections=5
 )
-
-st.write('Ingredientslist:', ingredients_list)
 
 if ingredients_list:
     ingredients_string = ''
@@ -37,7 +34,9 @@ if ingredients_list:
         ingredients_string += fruit_chosen + ' '
 
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        st.subheader(fruit_chosen + 'Nutrition Information')
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        st.write('search_on: ', search_on)
+      
         smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{search_on}")      
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)      
       
