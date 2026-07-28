a25d9a71efbbfaa9ef6d5ff56218bacf2dc6da79
rbaggen99 <raymond.baggen@home.nl>
2026-07-28 16:55:27 +0200

    Fix syntax error in fruit_options selection

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 pd_df = my_dataframe.to_pandas()
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME')
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
     , my_dataframe
