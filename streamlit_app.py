ca792b8153d44bcfd424bae9a6ad933df451cbca
rbaggen99 <raymond.baggen@home.nl>
2026-07-28 16:36:26 +0200

    Refactor API request for smoothie ingredients
    Updated the API request to use fruit_chosen directly instead of search_on.

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import os
+import requests
 
 # Write directly to the app
 st.title(f":cup_with_straw: Custom Smoothie Order Form :cup_with_straw:")
@@ -41,15 +42,12 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         
         st.success('Your Smoothie is ordered!', icon="✅")
-import requests
+
 if ingredients_list:
     ingredients_string = ''
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-
-        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-
         st.subheader(fruit_chosen + 'Nutrition Information')
-        smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{search_on}")      
+        smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)      
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)      
