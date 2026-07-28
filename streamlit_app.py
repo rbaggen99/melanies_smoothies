abfa3a21761a3865658a7017ccd5b1322316814f
rbaggen99 <raymond.baggen@home.nl>
2026-07-28 16:44:18 +0200

    Fix variable name in API request for fruit data

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,8 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
 
-        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+#        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+      
         my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
                     values ('""" + ingredients_string + """', '""" + name_on_order + """')"""
         time_to_insert = st.button('Submit Order')
@@ -49,5 +50,5 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
         st.subheader(fruit_chosen + 'Nutrition Information')
-        smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)      
+        smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{searcch_on}")      
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)      
